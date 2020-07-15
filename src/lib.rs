49d723972c1f647ad29f2b83141a1ea28906ac7c
Pavlos Karakalidis <pavloc.kara@outlook.com>
2020-07-15 13:31:15 +0300

    feat(lib): add PreSaveMut trait
    PreSaveMut is a new trait that gets mutable reference of the object
    that implements that trait. It will be used to mutate the underlying
    object before saving it to the database.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
+pub mod schemas;
+pub mod models;
+
 mod theinfinitytimes_lib {
     pub trait PreSave {
         fn pre_save(&self) {
             panic!("Abstract method");
         }
+    }
+    pub trait PreSaveMut{
+        fn pre_save(&mut self){ panic!("Abstract method")}
     }
 }
